47daf16ae67fb45af508094bbc42091f3ee56f58
zenitfan88 <nikanenkov@rambler.ru>
2022-06-12 14:15:25 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ public class Main {
         MonthlyPayment service = new MonthlyPayment();
 
         double payment = service.calculate(1_000_000, 12);
-        System.out.printf("Ежемесячный платеж - " + "%.0f%n", payment );
+        System.out.printf("Ежемесячный платеж - " + "%.0f%n", payment);
 
         double payment1 = service.calculate(1_000_000, 24);
-        System.out.printf("Ежемесячный платеж - " + "%.0f%n", payment1 );
+        System.out.printf("Ежемесячный платеж - " + "%.0f%n", payment1);
 
         double payment2 = service.calculate(1_000_000, 36);
-        System.out.printf("Ежемесячный платеж - " + "%.0f%n", payment2 );
+        System.out.printf("Ежемесячный платеж - " + "%.0f%n", payment2);
     }
 }
--- src/MonthlyPayment.java
+++ src/MonthlyPayment.java
@@ -1,11 +1,11 @@
 public class MonthlyPayment {
     public double calculate(int amount, int period) {
         double percent = 9.99;
-        double monthPercent = percent / (12*100) ;
-        double monthPercentPlusOne = monthPercent + 1 ;
+        double monthPercent = percent / (12 * 100);
+        double monthPercentPlusOne = monthPercent + 1;
         double monthPercentDegree = Math.pow(monthPercentPlusOne, period);
-        double ratio = (monthPercent*monthPercentDegree)/(monthPercentDegree-1);
-        double payment = amount*ratio;
+        double ratio = (monthPercent * monthPercentDegree) / (monthPercentDegree - 1);
+        double payment = amount * ratio;
         return payment;
     }
 }
